6fe0a5496774bbceef9a038536bc39de34cf9e92
MariaSurco <maria.surco@utec.edu.pe>
2022-11-21 23:09:57 -0500

    actu

--- rellenartablas.py
+++ rellenartablas.py
@@ -1,13 +1,34 @@
 from asyncio.windows_events import NULL
+from contextlib import nullcontext
 from faker import Faker
 from datetime import timedelta
 from random import randint, uniform
 import psycopg2
 
 
+def crear_organizacion(curs, fake):
+    print("run")
+    t=0
+    for _ in range(20):
+        try:
+            ruc = randint(100000,900000)
+            nombre = fake.last_name()
+            tipo = choice(['Institucion','Empresa'])
+            curs.execute(
+                f"INSERT INTO Organizacion(RUC,  Nombre, Tipo) VALUES('{ruc}','{nombre}','{tipo}');")
+        except Exception as e:
+            print(e)
+            t=t+1
+            print(t)
+            conn.rollback()
+        finally:
+            conn.commit()
+
+
 def crear_persona(curs, fake):
     print("run")
-    for _ in range(3):
+    t=0
+    for _ in range(90):
         try:
             dni = randint(10000000,99999999)
             nombre = fake.name()
@@ -17,36 +38,17 @@ def crear_persona(curs, fake):
                 f"INSERT INTO persona(dni,  apellidos, nombre) VALUES('{dni}','{apellido}','{nombre}');")
         except Exception as e:
             print(e)
-            print("i",_)
-            _-=1
-            print("i-1",_)
+            t=t+1
+            print(t)
             conn.rollback()
         finally:
             conn.commit()
-
-def crear_organizacion(curs, fake):
-    print("run")
-    for _ in range(30):
-        try:
-            ruc = randint(100000,900000)
-            nombre = fake.last_name()
-            tipo = choice(['Institucion','Empresa'])
-            curs.execute(
-                f"INSERT INTO Organizacion(RUC,  Nombre, Tipo) VALUES('{ruc}','{nombre}','{tipo}');")
-        except Exception as e:
-            print(e)
-            print("i",_)
-            _-=1
-            print("i-1",_)
-            conn.rollback()
-        finally:
-            conn.commit()
-
 
 def crear_trabajador(curs, fake):
     print("run")
+    t=0
     turnos = ["Dia","Tarde","Noche"]
-    for _ in range(18):
+    for _ in range(3):
         try:
             turno = turnos[randint(0,len(turnos)-1)]
             curs.execute('SELECT dni FROM persona ORDER BY RANDOM() LIMIT 1;')
@@ -79,16 +81,16 @@ def crear_trabajador(curs, fake):
                               VALUES('{dni_p[0][0]}','{sueldo}', '{turno}','{fecha_com}',NULL );""")
         except Exception as e:
             print(e)
-            print("i",_)
-            _-=1
-            print("i-1",_)
+            t=t+1
+            print(t)
             conn.rollback()
         finally:
             conn.commit()
 
 def crear_vendedor(curs,fake):
     print("run")
-    for _ in range(15):
+    t=0
+    for _ in range(1):
         try:
             curs.execute('SELECT dni FROM trabajador ORDER BY RANDOM() LIMIT 1;')
             dni_p = curs.fetchmany(1)
@@ -98,16 +100,16 @@ def crear_vendedor(curs,fake):
                               VALUES('{dni_p[0][0]}');""")
         except Exception as e:
             print(e)
-            print("i",_)
-            _-=1
-            print("i-1",_)
+            t=t+1
+            print(t)
             conn.rollback()
         finally:
             conn.commit()
 
 def crear_instalador(curs,fake):
     print("run")
-    for _ in range(15):
+    t=0
+    for _ in range(1):
         try:
             curs.execute('SELECT dni FROM trabajador ORDER BY RANDOM() LIMIT 1;')
             dni_p = curs.fetchmany(1)
@@ -117,16 +119,16 @@ def crear_instalador(curs,fake):
                               VALUES('{dni_p[0][0]}');""")
         except Exception as e:
             print(e)
-            print("i",_)
-            _-=1
-            print("i-1",_)
+            t=t+1
+            print(t)
             conn.rollback()
         finally:
             conn.commit()
 
 def crear_cliente(curs,fake):
     print("run")
-    for _ in range(15):
+    t=0
+    for _ in range(1):
         try:
             curs.execute('SELECT dni FROM persona ORDER BY RANDOM() LIMIT 1;')
             dni_p = curs.fetchmany(1)
@@ -136,18 +138,18 @@ def crear_cliente(curs,fake):
                               VALUES('{dni_p[0][0]}');""")
         except Exception as e:
             print(e)
-            print("i",_)
-            _-=1
-            print("i-1",_)
+            t=t+1
+            print(t)
             conn.rollback()
         finally:
             conn.commit()
 
 def create_gerenciado_porvend(curs,faker):
     print("run")
+    t=0
     gerentes_Vendedor=[50560365,90542850,51029504,74548110,73123844]
-    for x in range(5):
-        for y in range(3):
+    for x in range(1):
+        for y in range(1):
             try:
                 curs.execute('SELECT dni FROM vendedor ORDER BY RANDOM() LIMIT 1;')
                 dni_p = curs.fetchmany(1)
@@ -157,18 +159,18 @@ def create_gerenciado_porvend(curs,faker):
                                 VALUES('{gerentes_Vendedor[x-1]}','{dni_p[0][0]}');""")
             except Exception as e:
                 print(e)
-                print("i",y)
-                y-=1
-                print("i-1",y)
+                t=t+1
+                print(t)
                 conn.rollback()
             finally:
                 conn.commit()
 
-def create_gerenciado_porinsta(curs,faker):
+def create_gerenciado_porinsta(curs,faker):#instaladores 300 = 295
     print("run")
+    t=0
     gerentes_instalador=[56981582,98548293,43238999,29405006,50560345]
-    for x in range(5):#cambiar segun cantidad de datos
-        for y in range(1):#cambiar segun cantidad de datos
+    for x in range(2):#cambiar segun cantidad de datos
+        for y in range(2):#cambiar segun cantidad de datos
             try:
                 curs.execute('SELECT dni FROM instalador ORDER BY RANDOM() LIMIT 1;')
                 dni_p = curs.fetchmany(1)
@@ -178,16 +180,16 @@ def create_gerenciado_porinsta(curs,faker):
                                 VALUES('{gerentes_instalador[x-1]}','{dni_p[0][0]}');""")
             except Exception as e:
                 print(e)
-                print("i",y)
-                y-=1
-                print("i-1",y)
+                t=t+1
+                print(t)
                 conn.rollback()
             finally:
                 conn.commit()
 
 def create_venta(curs,fake):
     print("run")
-    for _ in range(15):
+    t=0
+    for _ in range(60):
         try:
             curs.execute('SELECT dni FROM vendedor ORDER BY RANDOM() LIMIT 1;')
             dni_p = curs.fetchmany(1)
@@ -198,9 +200,8 @@ def create_venta(curs,fake):
                                 VALUES('{venatid}','{dni_p[0][0]}','{direccion}');""")
         except Exception as e:
                 print(e)
-                print("i",_)
-                _-=1
-                print("i-1",_)
+                t=t+1
+                print(t)
                 conn.rollback()
         finally:
                 conn.commit()
@@ -208,7 +209,8 @@ def create_venta(curs,fake):
 
 def create_ventacorta(curs,fake):
     print("run")
-    for _ in range(1):
+    t=0
+    for _ in range(4):
         try:
             curs.execute('SELECT idcompra FROM venta ORDER BY RANDOM() LIMIT 1;')
             venta = curs.fetchmany(1)
@@ -219,9 +221,8 @@ def create_ventacorta(curs,fake):
                                 VALUES('{venta[0][0]}','{cliente[0][0]}');""")
         except Exception as e:
                 print(e)
-                print("i",_)
-                _-=1
-                print("i-1",_)
+                t=t+1
+                print(t)
                 conn.rollback()
         finally:
                 conn.commit()
@@ -229,7 +230,7 @@ def create_ventacorta(curs,fake):
 def create_ventalarga(curs,fake):
     print("run")
     t=0
-    for _ in range(2):
+    for _ in range(1):
         try:
             curs.execute('SELECT idcompra FROM venta ORDER BY RANDOM() LIMIT 1;')
             venta = curs.fetchmany(1)
@@ -241,9 +242,6 @@ def create_ventalarga(curs,fake):
                                 VALUES('{venta[0][0]}','{cliente[0][0]}','{duracion}');""")
         except Exception as e:
                 print(e)
-                print("i",_)
-                _-=1
-                print("i-1",_)
                 t=t+1
                 print(t)
                 conn.rollback()
@@ -286,9 +284,6 @@ def create_instalacion(curs,fake):
                                 VALUES('{venta[0][0]}','{fecha_com}','{instalador[0][0]}');""")
         except Exception as e:
                 print(e)
-                print("i",_)
-                _-=1
-                print("i-1",_)
                 t=t+1
                 print(t)
                 conn.rollback()
@@ -299,16 +294,16 @@ def create_instalacion(curs,fake):
 def create_necesita_usar(curs,fake):
     print("run")
     t=0
-    for x in range(2):
+    for x in range(1):
         try:
             curs.execute('SELECT idcompra FROM venta ORDER BY RANDOM() LIMIT 1;')
             venta = curs.fetchmany(1)
             curs.execute('SELECT nombre FROM producto ORDER BY RANDOM() LIMIT 1;')
-            producto=curs.fetchmany(1)
-            cantidad= randint(3,19)
+            cliente=curs.fetchmany(1)
+            duracion= randint(3,20)
             curs.execute(
                         f"""INSERT INTO necesita_usar(ventaid,producton,cantidad)
-                                VALUES('{venta[0][0]}','{producto[0][0]}','{cantidad}');""")
+                                VALUES('{venta[0][0]}','{cliente[0][0]}','{duracion}');""")
         except Exception as e:
             print(e)
                 
@@ -322,12 +317,19 @@ def create_necesita_usar(curs,fake):
 if __name__ == "__main__":
     
     fake = Faker()
-    conn_string = "dbname=prueba1 host=localhost password=123 user=postgres port=5432"
+    conn_string = "dbname=proyectobd host=localhost password=123 user=postgres port=5432"
     conn = psycopg2.connect(conn_string)
     with conn:
         with conn.cursor() as curs:
             #crear_instalador(curs,fake)
             #create_gerenciado_porinsta(curs,fake)
-            crear_persona(curs,fake)
-            #create_necesita_usar(curs,fake)
+            #crear_persona(curs,fake)
+            #crear_persona(curs,fake)
+            #crear_trabajador(curs,fake)
+            #crear_cliente(curs,fake)
+            #create_gerenciado_porvend(curs,fake)
+            #create_ventalarga(curs,fake)
+            #create_instalacion(curs,fake)
+            create_necesita_usar(curs,fake)
     conn.commit()
+
